7497cf6efaaab8d741981712465921fc8633e6a7
Mohit SONI <126953168+Mohit4122004@users.noreply.github.com>
2025-09-24 09:32:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'weather-app-ppx5.onrender.com' // 👈 add your deployed host here
+    ]
+  },
 })
